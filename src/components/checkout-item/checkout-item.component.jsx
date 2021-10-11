c9e03a098aa6c8846d5e7f9a1203444a198aa6b5
vladimir-bezrukov <web.dev903@gmail.com>
2021-10-11 17:55:11 +0200

    Adds checkout page functionality

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { clearItemFromCart } from '../../redux/cart/cart.actions';
+import {
+  clearItemFromCart,
+  addItem,
+  removeItem,
+} from '../../redux/cart/cart.actions';
 import './checkout-item.styles.scss';
 
-const CheckoutItem = ({ cartItem, clearItem }) => {
+const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
   const { name, imageUrl, price, quantity } = cartItem;
   return (
     <div className='checkout-item'>
@@ -12,11 +16,11 @@ const CheckoutItem = ({ cartItem, clearItem }) => {
       </div>
       <span className='name'>{name}</span>
       <span className='quantity'>
-        <div className='arrow' onClick={() => clearItem(cartItem)}>
+        <div className='arrow' onClick={() => removeItem(cartItem)}>
           &#10094;
         </div>
         <span className='value'>{quantity}</span>
-        <div className='arrow' onClick={() => clearItem(cartItem)}>
+        <div className='arrow' onClick={() => addItem(cartItem)}>
           &#10095;
         </div>
       </span>
@@ -29,7 +33,9 @@ const CheckoutItem = ({ cartItem, clearItem }) => {
 };
 
 const mapStateToProps = (dispatch) => ({
-  clearItem: (item) => dispatch({ type: 'CLEAR_ITEM', payload: item }),
+  clearItem: (item) => dispatch(clearItemFromCart(item)),
+  addItem: (item) => dispatch(addItem(item)),
+  removeItem: (item) => dispatch(removeItem(item)),
 });
 
 export default connect(null, mapStateToProps)(CheckoutItem);
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import CartActionTypes from './cart.types';
-import { addItemToCart } from './cart.utils';
+import { addItemToCart, removeItemFromCart } from './cart.utils';
 
 const INITIAL_STATE = {
   hidden: true,
@@ -18,6 +18,23 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         ...state,
         cartItems: addItemToCart(state.cartItems, action.payload),
       };
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          (cartItem) => cartItem.id !== action.payload.id
+        ),
+      };
+    case CartActionTypes.REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, action.payload),
+      };
+    case CartActionTypes.CLEAR_CART:
+      return {
+        ...state,
+        cartItems: [],
+      };
 
     default:
       return state;
@@ -25,22 +42,3 @@ const cartReducer = (state = INITIAL_STATE, action) => {
 };
 
 export default cartReducer;
-// case CartActionTypes.CLEAR_ITEM_FROM_CART:
-//   return {
-//     ...state,
-//     cartItems: state.cartItems.filter(
-//       (cartItem) => cartItem.id !== action.payload.id
-//     ),
-//   };
-// case CartActionTypes.REMOVE_ITEM:
-//   return {
-//     ...state,
-//     cartItems: state.cartItems.filter(
-//       (cartItem) => cartItem.id !== action.payload.id
-//     ),
-//   };
-// case CartActionTypes.CLEAR_CART:
-//   return {
-//     ...state,
-//     cartItems: [],
-//   };
