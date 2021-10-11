7a5824053289d91af2db03a46c6b6b3f1907f1c7
vladimir-bezrukov <web.dev903@gmail.com>
2021-10-11 12:00:28 +0200

    Adds Redux

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { auth } from '../../firebase/firebase.utils';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
@@ -28,4 +29,8 @@ const Header = ({ currentUser }) => (
     </div>
   </div>
 );
-export default Header;
+
+const mapStateToProps = ({ user: { currentUser } }) => ({
+  currentUser,
+});
+export default connect(mapStateToProps)(Header);
