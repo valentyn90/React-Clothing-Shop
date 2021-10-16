342b62f487889c1a9d972365f4b55d6b5ca9d221
vladimir-bezrukov <web.dev903@gmail.com>
2021-10-16 16:57:06 +0200

    REmoves manifest

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import { PersistGate } from 'redux-persist/integration/react';
 
 import { store, persistor } from './redux/store';
 
-import './index.css';
 import App from './App';
 
 ReactDOM.render(
